839df570a3bb0d8c10c55a6da0c2c0d50cbea02c
dawangraoming <dawangraoming@hotmail.com>
2022-12-09 02:22:30 +0800

    feat(utils): :necktie: add env util

---
+++ src/utils/env.ts
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import { z } from 'zod';
+
+config();
+
+const envFiles = ['.env', '.env.local'];
+
+const envSchema = z.object({
+  TELEGRAM_BOT_TOKEN: z.string().min(5),
+  CHATGPT_TOKEN: z.string().min(5),
+});
+
+export const env = envSchema.parse(process.env);
+
+export const isDev = process.env.NODE_ENV === 'development';
+
+export const isProd = !isDev;
+
+export default env;
