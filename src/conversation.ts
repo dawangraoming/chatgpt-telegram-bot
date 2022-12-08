3347f95a8ef9f8653b5c06cb93a6d14d324f762a
dawangraoming <dawangraoming@hotmail.com>
2022-12-09 02:23:07 +0800

    feat: :necktie: add conversation module

---
+++ src/conversation.ts
@@ -0,0 +1,34 @@
+import { ChatGPTAPI, ChatGPTConversation } from 'chatgpt';
+import { env } from './utils/env';
+
+// store conversation
+const memory = new Map<string, ChatGPTConversation>();
+
+const api = new ChatGPTAPI({
+  sessionToken: env.CHATGPT_TOKEN,
+});
+
+/**
+ * send message to chatGPT
+ */
+export const send = async (id: number | string, context: string) => {
+  const sId = id.toString();
+  let conversation = memory.get(sId);
+
+  if (!conversation) {
+    conversation = api.getConversation();
+    memory.set(sId, conversation);
+  }
+
+  return conversation.sendMessage(context);
+};
+
+/**
+ * create a new conversation
+ */
+export const create = (id: number | string) => {
+  const sId = id.toString();
+  const conversation = api.getConversation();
+  memory.set(sId, conversation);
+  return conversation;
+};
